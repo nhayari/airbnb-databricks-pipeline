b0ce1312bd0b6d0083081e4feacd41fe4ab92263
nour <46919831+nhayari@users.noreply.github.com>
2026-03-13 16:44:30 +0100

    add jobs

--- workflows/job_airbnb.py
+++ workflows/job_airbnb.py
@@ -1,1 +1,64 @@
+﻿# Upgrade Databricks SDK to the latest version and restart Python to see updated packages
+%pip install --upgrade databricks-sdk==0.70.0
+%restart_python
 
+from databricks.sdk.service.jobs import JobSettings as Job
+
+
+airbnb_pipeline = Job.from_dict(
+    {
+        "name": "airbnb_pipeline",
+        "email_notifications": {
+            "on_success": [
+                "myemail@???.com",
+            ],
+            "on_failure": [
+                "myemail@???.com",
+            ],
+        },
+        "schedule": {
+            "quartz_cron_expression": "42 30 2 * * ?",
+            "timezone_id": "Europe/Paris",
+            "pause_status": "PAUSED",
+        },
+        "tasks": [
+            {
+                "task_key": "download",
+                "run_job_task": {
+                    "job_id": 142753620867121,
+                },
+            },
+            {
+                "task_key": "Bronze",
+                "depends_on": [
+                    {
+                        "task_key": "download",
+                    },
+                ],
+                "run_job_task": {
+                    "job_id": 481158249657270,
+                },
+            },
+            {
+                "task_key": "Silver",
+                "depends_on": [
+                    {
+                        "task_key": "Bronze",
+                    },
+                ],
+                "run_job_task": {
+                    "job_id": 186406315997448,
+                },
+            },
+        ],
+        "queue": {
+            "enabled": True,
+        },
+    }
+)
+
+from databricks.sdk import WorkspaceClient
+
+w = WorkspaceClient()
+w.jobs.reset(new_settings=airbnb_pipeline, job_id=997336968762480)
+# or create a new job using: w.jobs.create(**airbnb_pipeline.as_shallow_dict())
---
+++ workflows/job_bronze_to_silver.py
@@ -0,0 +1,73 @@
+﻿# Upgrade Databricks SDK to the latest version and restart Python to see updated packages
+%pip install --upgrade databricks-sdk==0.70.0
+%restart_python
+
+from databricks.sdk.service.jobs import JobSettings as Job
+
+
+bronze_to_silver = Job.from_dict(
+    {
+        "name": "bronze_to_silver",
+        "tasks": [
+            {
+                "task_key": "calendar",
+                "sql_task": {
+                    "file": {
+                        "path": "/Workspace/Users/myemail@???.???/airbnb-databricks-pipeline/silver/calendar.sql",
+                        "source": "WORKSPACE",
+                    },
+                    "warehouse_id": "4a91db18b293ba94",
+                },
+            },
+            {
+                "task_key": "listings",
+                "sql_task": {
+                    "file": {
+                        "path": "/Workspace/Users/myemail@???.???/airbnb-databricks-pipeline/silver/listings.sql",
+                        "source": "WORKSPACE",
+                    },
+                    "warehouse_id": "4a91db18b293ba94",
+                },
+            },
+            {
+                "task_key": "listings_details",
+                "sql_task": {
+                    "file": {
+                        "path": "/Workspace/Users/myemail@???.???/airbnb-databricks-pipeline/silver/listings_details.sql",
+                        "source": "WORKSPACE",
+                    },
+                    "warehouse_id": "4a91db18b293ba94",
+                },
+            },
+            {
+                "task_key": "neighberhoods",
+                "sql_task": {
+                    "file": {
+                        "path": "/Workspace/Users/myemail@???.???/airbnb-databricks-pipeline/silver/neighbourhoods_geo.sql",
+                        "source": "WORKSPACE",
+                    },
+                    "warehouse_id": "4a91db18b293ba94",
+                },
+            },
+            {
+                "task_key": "reviews_details",
+                "sql_task": {
+                    "file": {
+                        "path": "/Workspace/Users/myemail@???.???/airbnb-databricks-pipeline/silver/reviews_details.sql",
+                        "source": "WORKSPACE",
+                    },
+                    "warehouse_id": "4a91db18b293ba94",
+                },
+            },
+        ],
+        "queue": {
+            "enabled": True,
+        },
+    }
+)
+
+from databricks.sdk import WorkspaceClient
+
+w = WorkspaceClient()
+w.jobs.reset(new_settings=bronze_to_silver, job_id=186406315997448)
+# or create a new job using: w.jobs.create(**bronze_to_silver.as_shallow_dict())
